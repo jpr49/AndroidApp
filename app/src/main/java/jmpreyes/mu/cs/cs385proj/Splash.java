c80ff1fff6e4f6e61d563a9b8551769e73487531
JosephR <jreyes525@hotmail.com>
2018-01-20 13:15:33 +0000

    Latest on Jan 20

---
+++ app/src/main/java/jmpreyes/mu/cs/cs385proj/Splash.java
@@ -0,0 +1,30 @@
+package jmpreyes.mu.cs.cs385proj;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000); // Sleep for 3 seconds
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    // Intent to open another activity
+                    Intent intent = new Intent(Splash.this, Login.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
